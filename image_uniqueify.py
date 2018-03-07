bf79e2b75f3d2ee4b1f15ad2c2dfab2cdf4964e7
Colm Carew <colm.carew@distilled.ie>
2018-03-07 15:56:22 +0000

    Getting a Hash of each image

--- image_uniqueify.py
+++ image_uniqueify.py
@@ -1,14 +1,24 @@
 from os import walk
+import sys
+import hashlib
 
-dirpath = "/Users/colmcarew/IdeaProjects/cachingrest/images"
+BUF_SIZE = 65536
+DIRPATH = "/Users/colmcarew/IdeaProjects/cachingrest/images"
 file_list = []
 
 def populateFileList():
-	for(directory, dirnames, filenames) in walk(dirpath):
+	for(directory, dirnames, filenames) in walk(DIRPATH):
 		for filename in filenames:
 			file_list.append(directory + "/" + filename)
 
 
 populateFileList()
 for s in file_list :
-	print(s)
+	md5 = hashlib.md5()
+	with open(s, 'rb') as file :
+		while(True):
+			data = file.read(BUF_SIZE)
+			if not data:
+				break
+			md5.update(data)
+	print(s, " ", "MD5: {0}".format(md5.hexdigest()))
